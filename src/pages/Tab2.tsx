6f976f132b4d1bfc131465bd99251900de267430
Sebastian H <SebastianHelguera@rafaels-imac.attlocal.net>
2023-11-25 13:36:45 -0800

    first commit

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,15 +1,41 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { camera, trash, close } from 'ionicons/icons';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+  IonActionSheet,
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
 
 const Tab2: React.FC = () => {
+  const { photos, takePhoto } = usePhotoGallery();
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Photo Gallery</IonTitle>
         </IonToolbar>
       </IonHeader>
+      <IonGrid>
+          <IonRow>
+          {photos.map((photo, index) => (
+        <IonCol size="6" key={photo.filepath}>
+          <IonImg src={photo.webviewPath} />
+        </IonCol>
+      ))}
+          </IonRow>
+        </IonGrid>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
@@ -18,6 +44,13 @@ const Tab2: React.FC = () => {
         </IonHeader>
         <ExploreContainer name="Tab 2 page" />
       </IonContent>
+      <IonContent>
+  <IonFab vertical="bottom" horizontal="center" slot="fixed">
+    <IonFabButton onClick={() => takePhoto()}>
+      <IonIcon icon={camera}></IonIcon>
+    </IonFabButton>
+  </IonFab>
+</IonContent>
     </IonPage>
   );
 };
